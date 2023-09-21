2f59757f84ec19fcadbc2a952dc7fc1ec849a458
Yuri A. Nascimento <yurinascimento@outlook.com>
2023-09-20 21:20:22 -0300

    Adição de 2 testes unitários

---
+++ ParOuImparChecker/Models/Dto/VerificarParidadeResultDto.cs
@@ -0,0 +1,16 @@
+﻿namespace ParOuImparChecker.Models.Dto
+{
+    public class VerificarParidadeResultDto
+    {
+        public VerificarParidadeResultDto()
+        {
+        }
+
+        public string Mensagem { get; set; }
+        public bool Vitoria { get; set; }
+        public int NumeroEntrada { get; set; }
+        public string Aposta { get; set; }
+        public int NumeroAleatorioGerado { get; set; }
+        public int Soma { get; set; }
+    }
+}
--- ParOuImparCheckerTests/Controllers/VerificarParidadeControllerTests.cs
+++ ParOuImparCheckerTests/Controllers/VerificarParidadeControllerTests.cs
@@ -1,35 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParOuImparChecker.Controllers;
 using Xunit;
 
-namespace ParOuImparChecker.Controllers.Tests
+namespace ParOuImparChecker.Tests.Controllers
 {
     public class VerificarParidadeControllerTests
     {
-        [Fact]
-        public void VerificarParidade_ParametrosValidos_DeveRetornarOkObject()
-        {
-            // Arrange
-            var controller = new VerificarParidadeController();
 
-            // Act
-            var resultado = controller.VerificarParidade(2, "par");
-
-            // Assert
-            Assert.IsInstanceOfType(resultado.GetType(), typeof(OkObjectResult));
-        }
-
-        [Fact]
-        public void VerificarParidade_ParametrosInvalidos_DeveRetornarBadRequest()
-        {
-            // Arrange
-            var controller = new VerificarParidadeController();
-
-            // Act
-            var resultado = controller.VerificarParidade(6, "invalido");
-
-            // Assert
-            Assert.IsInstanceOfType(resultado.GetType(), typeof(BadRequestResult));
-        }
     }
 }
