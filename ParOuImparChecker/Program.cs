ef9aae590b743b960cbf379c5cb857126681cf3f
Yuri A. Nascimento <yurinascimento@outlook.com>
2023-09-21 23:58:18 -0300

    Add Health Check

--- ParOuImparChecker/Program.cs
+++ ParOuImparChecker/Program.cs
@@ -6,6 +6,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
+
+builder.Services.AddHealthChecks();
+builder.Services
+    .AddHealthChecksUI(setupSettings: setup =>
+    {
+        setup.AddHealthCheckEndpoint("Swagger Api", "/api/verificar-paridade?numeroEntrada=4&parOuImpar=par");
+    });
+
+builder.Services.AddHealthChecksUI().AddInMemoryStorage();
 
 var app = builder.Build();
 
@@ -19,4 +29,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app
+    .UseRouting()
+    .UseEndpoints(config => config.MapHealthChecksUI());
+
 app.Run();
+
+
